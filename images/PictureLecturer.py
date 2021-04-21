d769726cc25ae8e014289555586ce2793596dfcc
ArthurVieillevoye <56159647+ArthurVieillevoye@users.noreply.github.com>
2021-04-21 12:20:04 +0200

    image reader

---
+++ images/PictureLecturer.py
@@ -0,0 +1,20 @@
+import cv2
+import os
+
+
+def load_images_from_folder(folder):
+    imagesArray = []
+    print('hello')
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder, filename))
+        if img is not None:
+            imagesArray.append(img)
+    return imagesArray
+
+
+images = load_images_from_folder(os.path.abspath(os.getcwd()))
+
+for image in images:
+    cv2.imshow('Picture', image)
+    cv2.waitKey(0)
+    cv2.destroyWindow('Picture')
