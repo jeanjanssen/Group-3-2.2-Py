2cfc43b6fe9df707f06a1e3a117a8fd2d4e5d073
ArthurVieillevoye <56159647+ArthurVieillevoye@users.noreply.github.com>
2021-06-10 13:07:08 +0200

    Trained classifier

--- projectLocation.py
+++ projectLocation.py
@@ -18,4 +18,11 @@ generate_negative_description_file()
 
 print(returnLocation())
 
+# To create the positive images
 #"C:/Users/Arthur Vieillevoye/Downloads/opencv/build/x64/vc15/bin/opencv_annotation.exe" --annotations=pos.txt --images=Positives/
+
+#To sample in the positive images:
+#"C:/Users/Arthur Vieillevoye/Downloads/opencv/build/x64/vc15/bin/opencv_createsamples.exe" -info pos.txt -w 24 -h 24 -num 1000 -vec pos.vec
+
+#Training the xml file:
+#"C:/Users/Arthur Vieillevoye/Downloads/opencv/build/x64/vc15/bin/opencv_traincascade.exe" -data OPencvTraining/ -vec pos.vec -bg neg.txt -numPos 500 -numNeg 250 -numStages 12 -w 24 -h 24
